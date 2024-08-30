f9cce22b9404038668db72f00ec692ff8f18637a
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-30 12:21:54 -0400

    start a server

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import swaggerUI from "swagger-ui-express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(cors());
+dotenv.config();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+
+mongoose
+	.connect(process.env.MONGO_DB_URI)
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.error("Error with MongoDB Connection", err));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
